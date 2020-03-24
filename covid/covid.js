48a4b96ca3715ce611e39fdfe31979449829d692
regan <regan.maharjan@olenepal.org>
2020-03-24 11:58:06 +0545

    covid-2

--- covid/covid.js
+++ covid/covid.js
@@ -13,7 +13,7 @@ $('.button1').click(function(){
 $('.button2').click(function(){  
     $('.map-stat').css({'opacity':'1', 'z-index':'2'});
     $('.update-info').css({'z-index':'1','opacity':'0'});
-    $(this).css({'box-shadow': '-9px -4px 5px 1px rgba(0,0,0,0.18)', 'background':'white', 'z-index':'3', 'background':'white'});
+    $(this).css({'box-shadow': '-6px 1px 5px -3px rgba(0,0,0,0.18)', 'background':'white', 'z-index':'3', 'background':'white'});
     $('.map-stat').css({'box-shadow': '-11px -18px 29px -17px rgba(0,0,0,0.19)', 'z-index':'2'});
     $('.button1').css({'background':'#6BCDFD', 'z-index':'2'});
 });
